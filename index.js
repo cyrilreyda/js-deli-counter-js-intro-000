eb986a2cf40ccfd7c4fe8e08468742a814d15633
Cyril Reida <creyda@gmail.com>
2019-04-14 18:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ function takeANumber(currentLine, newName) {
   return `Welcome, ${newName}. You are number ${currentLine} in line.` 
 }
 
-function nowServing(katzDeliLine)
+function nowServing(katzDeliLine) {
+  
+  if (katzDeliLine.length) {
+    return 
+  }
+  else {return "There is nobody waiting to be served!"}
+  
+  
+}
