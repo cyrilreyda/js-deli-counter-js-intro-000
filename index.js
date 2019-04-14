53f5c1f148e062bc74adf3b0d7d9326471af5a6e
Cyril Reida <creyda@gmail.com>
2019-04-14 18:19:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ function takeANumber(currentLine, newName) {
   return `Welcome, ${newName}. You are number ${currentLine} in line.` 
 }
 
+
 function nowServing(katzDeliLine) {
   
   if (katzDeliLine.length) {
     return katzDeliLine.shift();
   }
   else {return "There is nobody waiting to be served!"}
-  
- function currentLine(katzDeliLine) {
-   for (let i = 0; i < katzDeliLine.length; i++) {
-     return "The line is currently:
-   }
- };
 }
+
+
+function currentLine(katzDeliLine) {
+   if (katzDeliLine.length) {
+     
+      for (let i = 0; i < katzDeliLine.length; i++) {
+         return "The line is currently:
+       }
+       
+   } else {return "The line is currently empty."}
+} 
